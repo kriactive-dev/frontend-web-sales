3d296068cce753059ecc2cd41891f44df1db3cf7
34sea <domingosmartinho67@gmail.com>
2025-08-21 09:41:18 +0200

    adding load layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,24 +1,58 @@
 import Sidebar from "../components/dashboard/Sidebar";
 import Header from "../components/dashboard/Header";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Dashboard: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   const toggleSidebar = () => {
+    
     setSidebarOpen(prev => !prev);
+    console.log("Gravancao")
+    console.log(sidebarOpen)
+    localStorage.setItem("menuopenkriative", JSON.stringify(sidebarOpen));
   };
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const dados = localStorage.getItem("menuopenkriative");
+
+      if (dados) {
+        const isTrue = dados.toString().toLowerCase() === "true";
+        setSidebarOpen(!isTrue); 
+      }
+
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="containerLoader">
+        <div className="loader"></div>
+    </div>
+    );
+  }
+
   return (
     <div className="barsDash">
-      <div className={sidebarOpen ? "barsHeaderSide" : "sidebarhiden"} onClick={() => {
-        if (sidebarOpen == false) {
-          setSidebarOpen(true)
-        }
-      }}>
-        <div className="sidebar" onClick={(e) => {
-          e.stopPropagation();
-        }}>
-          <Sidebar onToggleSidebar={toggleSidebar}/>
+      <div
+        className={sidebarOpen ? "barsHeaderSide" : "sidebarhiden"}
+        onClick={() => {
+          if (!sidebarOpen) setSidebarOpen(true);
+        }}
+      >
+        <div
+          className="sidebar"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <Sidebar onToggleSidebar={toggleSidebar} />
         </div>
       </div>
       <div className={sidebarOpen ? "barsBody" : "barsBodyAll"}>
@@ -32,5 +66,6 @@ const Dashboard: React.FC = () => {
     </div>
   );
 }
+
 
 export default Dashboard;
